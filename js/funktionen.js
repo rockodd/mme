870df9f82673b27a5ab87c28f82f3b933909a011
rockodd <rocko_1986@hotmail.com>
2016-01-10 19:03:45 +0100

    tabelle workt

--- js/funktionen.js
+++ js/funktionen.js
@@ -1,8 +1,6 @@
-﻿
-
-// Neuen Datensatz erstellen über Aside Menü
+﻿// Neuen Datensatz erstellen �ber Aside Men�
 newdatabox = document.getElementById("pencil");
-newdatabox.addEventListener('click', function(){
+newdatabox.addEventListener('click', function () {
     InfoFenster();
 });
 
@@ -15,172 +13,154 @@ setTimeout("Bildwechsel(0)", 3000);
 
 function Bildwechsel(x) {
     document.getElementById("Foto01").src = images[x];
-    timeoutId = setTimeout(function() {
+    timeoutId = setTimeout(function () {
         Bildwechsel((x + 1) % images.length)
     }, 3000);
 }
 
 
-
-
 // Einbindung von google Maps
-    window.addEventListener('load',initialize);
-    function initialize() {
-      var mapProp = {
-      center:new google.maps.LatLng(52.544516,13.354815),
-      zoom:16,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-    
+window.addEventListener('load', initialize);
+function initialize() {
+    var mapProp = {
+        center: new google.maps.LatLng(52.544516, 13.354815),
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
-
-//Formulareingaben überprüfen
-window.addEventListener("load",initialisiereFunktion,false);
-function initialisiereFunktion(){
+//Formulareingaben �berpr�fen
+window.addEventListener("load", initialisiereFunktion, false);
+function initialisiereFunktion() {
     submitBtn = document.getElementById("submitbtn");
-    submitBtn.addEventListener('click', function(){
+    submitBtn.addEventListener('click', function () {
         return submitPruefen();
     });
 }
-function submitPruefen(){
+function submitPruefen() {
     var f = document.Formular;
     var fehler = "";
     var pattName = new RegExp("[A-Za-z]{3}")
 
-    if(document.Formular.Name.value == "" || (!pattName.test(document.Formular.Name.value) ))
-    {
+    if (document.Formular.Name.value == "" || (!pattName.test(document.Formular.Name.value) )) {
         fehler += "-Bezeichnung\n";
         document.Formular.Name.focus();
-    
+
     }
 
-    if(document.Formular.Subtext.value == "" || (!pattName.test(document.Formular.Subtext.value)) )
-    {
+    if (document.Formular.Subtext.value == "" || (!pattName.test(document.Formular.Subtext.value))) {
         fehler += "-Bemerkung\n";
         document.Formular.Subtext.focus();
-    
+
     }
 
-    if(document.Formular.Date.value == "" || document.Formular.Date.value.length > 10)
-    {
+    if (document.Formular.Date.value == "" || document.Formular.Date.value.length > 10) {
         fehler += "-Datum\n";
         document.Formular.Date.focus();
-        
+
     }
-    if(document.Formular.Start.value == "")
-    {
+    if (document.Formular.Start.value == "") {
         fehler += "-Start\n";
         document.Formular.Start.focus();
-        
+
     }
-    if(document.Formular.End.value == "")
-    {
+    if (document.Formular.End.value == "") {
         fehler += "-Ende\n";
         document.Formular.End.focus();
-        
+
     }
-    if (fehler !="") {
+    if (fehler != "") {
         var fehlertext = "Die folgenden Felder wurden nicht korrekt ausgefüllt:\n\n";
         fehlertext += fehler;
-        /*alert(fehlertext + "\nBitte füllen Sie die Informationen noch aus. Danke.");*/
-        document.getElementById("resultLabel").innerHTML="Bitte alle Felder ausfüllen";
+        /*alert(fehlertext + "\nBitte f�llen Sie die Informationen noch aus. Danke.");*/
+        document.getElementById("resultLabel").innerHTML = "Bitte alle Felder ausfüllen";
         return false;
-        }
-        document.getElementById("resultLabel").innerHTML="Eingabe gespeichert";
-        setTimeout(closeOverlay(),3000);
+    }
+    document.getElementById("resultLabel").innerHTML = "Eingabe gespeichert";
+    setTimeout(closeOverlay(), 3000);
 
-        }
+}
 
 
-        
-
-// Prüfen der Eingabe für Beschreibung sonst ErrorLabel einblenden
+// Pr�fen der Eingabe f�r Beschreibung sonst ErrorLabel einblenden
 n = document.getElementById("NameText");
-n.addEventListener('change', function(){
+n.addEventListener('change', function () {
     var patt = new RegExp("[A-Za-z]{3}");
-    if(document.Formular.Name.value == "" || (!patt.test(document.Formular.Name.value) ))
-    {
-        document.getElementById("bezLabel").innerHTML="min. 3 Buchstaben verwenden";
+    if (document.Formular.Name.value == "" || (!patt.test(document.Formular.Name.value) )) {
+        document.getElementById("bezLabel").innerHTML = "min. 3 Buchstaben verwenden";
     }
-    else document.getElementById("bezLabel").innerHTML="";
+    else document.getElementById("bezLabel").innerHTML = "";
 });
 
-// Prüfen der Eingabe für Bemerkung sonst ErrorLabel einblenden
+// Pr�fen der Eingabe f�r Bemerkung sonst ErrorLabel einblenden
 s = document.getElementById("subtext");
-s.addEventListener('change', function(){
+s.addEventListener('change', function () {
     var patt = new RegExp("[A-Za-z]{3}")
-    if(document.Formular.Subtext.value == "" || (!patt.test(document.Formular.Subtext.value) ))
-    {
-        document.getElementById("subLabel").innerHTML="min. 3 Buchstaben verwenden";
+    if (document.Formular.Subtext.value == "" || (!patt.test(document.Formular.Subtext.value) )) {
+        document.getElementById("subLabel").innerHTML = "min. 3 Buchstaben verwenden";
     }
-    else document.getElementById("subLabel").innerHTML="";
+    else document.getElementById("subLabel").innerHTML = "";
 });
 
-// Prüfen der Eingabe für Date sonst ErrorLabel einblenden
+// Pr�fen der Eingabe f�r Date sonst ErrorLabel einblenden
 dateID = document.getElementById("date");
-dateID.addEventListener('change', function(){
+dateID.addEventListener('change', function () {
 
-    if(document.Formular.Date.value == "" || document.Formular.Date.value.length > 10)
-    {
-        document.getElementById("dateLabel").innerHTML="falsches Datumsformat";
+    if (document.Formular.Date.value == "" || document.Formular.Date.value.length > 10) {
+        document.getElementById("dateLabel").innerHTML = "falsches Datumsformat";
     }
-    else document.getElementById("dateLabel").innerHTML="";
+    else document.getElementById("dateLabel").innerHTML = "";
 });
 
-// Prüfen der Eingabe für Startzeit sonst ErrorLabel einblenden
+// Pr�fen der Eingabe f�r Startzeit sonst ErrorLabel einblenden
 startID = document.getElementById("start");
-startID.addEventListener('change', function(){
+startID.addEventListener('change', function () {
 
-    if(document.Formular.Start.value == "")
-    {
-        document.getElementById("startLabel").innerHTML="falsche Zeitangabe";
+    if (document.Formular.Start.value == "") {
+        document.getElementById("startLabel").innerHTML = "falsche Zeitangabe";
     }
-    else document.getElementById("startLabel").innerHTML="";
+    else document.getElementById("startLabel").innerHTML = "";
 });
 
-// Prüfen der Eingabe für Startzeit sonst ErrorLabel einblenden
+// Pr�fen der Eingabe f�r Startzeit sonst ErrorLabel einblenden
 endID = document.getElementById("end");
-endID.addEventListener('change', function(){
+endID.addEventListener('change', function () {
 
-    if(document.Formular.End.value == "")
-    {
-        document.getElementById("endLabel").innerHTML="falsche Zeitangabe";
+    if (document.Formular.End.value == "") {
+        document.getElementById("endLabel").innerHTML = "falsche Zeitangabe";
     }
-    else document.getElementById("endLabel").innerHTML="";
+    else document.getElementById("endLabel").innerHTML = "";
 });
 
 
-//Navigationsleiste führt durch mit scrollen mit
-window.onscroll = function() {
+// Navigationsleiste f�hrt durch mit scrollen mit
+window.onscroll = function () {
     var menu = document.getElementById('menu');
-    if( document.body.scrollTop+document.documentElement.scrollTop > 30)
+    if (document.body.scrollTop + document.documentElement.scrollTop > 30)
 
         menu.className = "stuck";
     else menu.className = "";
 };
 
 
-
-
 function InfoFenster() {
     var overlay = document.getElementById('overlay');
     var specialBox = document.getElementById('Box');
     overlay.style.opacity = .8;
-        overlay.style.display = "block";
-        specialBox.style.display = "block";
+    overlay.style.display = "block";
+    specialBox.style.display = "block";
 }
 
+// Overlayfenster schlie�en
 c = document.getElementById('Close');
 c.addEventListener('click', closeOverlay, false);
-
-    function closeOverlay() {
-        var overlay = document.getElementById('overlay');
-        var specialBox = document.getElementById('Box');
-        overlay.style.display = "none";
-        specialBox.style.display = "none";
-    };
+function closeOverlay() {
+    var overlay = document.getElementById('overlay');
+    var specialBox = document.getElementById('Box');
+    overlay.style.display = "none";
+    specialBox.style.display = "none";
+};
 

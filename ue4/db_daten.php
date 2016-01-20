b26eee5afb8dc45763f583a526457da12f5ad3a4
rockodd <rocko_1986@hotmail.com>
2016-01-20 19:32:25 +0100

    add with html and php

--- ue4/db_daten.php
+++ ue4/db_daten.php
@@ -1,11 +1,11 @@
 
 <!--Dieses Skript stellt die Verbindungsdaten zur Datenbank bereit--> 
-2 <?php 
+<?php
  	// Die Attribute des Konstruktors eines MySQLi-Objektes: Datenbankserver, Benutzername, Passwort und Datenbank 
  	$mysqli = new mysqli("localhost", "root", "", "mme"); 
 	if($mysqli->connect_error) { 
  		echo "<script>alert('Verbindung zur Datenbank konnte nicht hergestellt werden.')</script>"; 
  	} 
-8 ?> 
+?>
 
 
---
+++ ue4/tmp_bearbeiten.php
@@ -0,0 +1,81 @@
+<?php
+	require_once "db_daten.php";
+	if(isset($_GET["id"]) && isset($_GET["action"])) {
+?>
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<title>Tolle Tabelle</title>
+		<meta charset="utf-8">
+		<meta id="#" content="#">
+		<link rel="stylesheet" href="../../../Dropbox/Studium/Studium%20-%20Medieninformatik/!semester%2003/Multimedia_Engineering_I/Brainstorm/MME-Uebung-Tobias/Übung4/projekt4/css/merge.css">
+		<script src=""></script>
+	</head>
+	<body>
+		<!--action="<?php// echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"-->
+		<form method="post" action="tmp_submit.php">
+			<table border='1'>
+			<tr><th>Etappe:</th><th>Region:</th><th>Datum:</th><th>Kommentar:</th><th></th><th></th></tr>
+			<?php
+				require_once "db_daten.php"; 																						// Verbindungsdaten zur Datenbank
+				$sql_abfrage = $mysqli->query("SELECT id, etappe, region, datum, kommentar FROM route;"); 							// SQL-Abfrage
+						while($datensatz = $sql_abfrage->fetch_array()) {
+							$id			= htmlspecialchars($datensatz['id']);
+							$etappe		= htmlspecialchars($datensatz['etappe']);
+							$region		= htmlspecialchars($datensatz['region']);
+							$datum		= htmlspecialchars($datensatz['datum']);
+							$kommentar	= htmlspecialchars($datensatz['kommentar']);
+							if($_GET["id"] == $id) {
+								echo "<tr>"
+									   ?> <input name="id" type='hidden' value="<?php echo $id;		?>")> <?php echo 
+									   "<td>" ?> <input name="etappe" type='text' value="<?php echo $etappe;		?>")> <?php echo "</td>"
+//									 . "<td>" ?> <input name="region" type='text' value="<?php echo $region;		?>")> <?php echo "</td>"
+									 . "<td>" ?> .	<select name="region">
+														<?php
+															$nordinsel	= array("Auckland", "Northland", "Coromandel Peninsula", "Waikato und King Country", "Bay Of Plenty", "East Coast", "Zentrale Hochebene",
+																			  "Taranaki", "Whanganui und Palmerston North", "Wellington");
+															$suedinsel	= array("Marlborough und Nelson", "Christchurch und Canterbury", "Otago", "Fjordland und Southland",
+																			  "Queenstown", "West Coast");
+
+															//echo "<option selected>$region</option>";							//Platzhalter
+															echo "<optgroup label='Nordinsel'>";								// Unterteilung
+															foreach($nordinsel as $element) {
+																if($element == $region) echo "<option value='$element' selected>$element</option>\n";
+																else 					echo "<option value='$element'>			$element</option>\n";
+															}
+															echo "</optgroup>";
+															echo "<optgroup label='Südinsel'>";									// Unterteilung
+															foreach($suedinsel as $element) {
+																if($element == $region) echo "<option value='$element' selected>$element</option>\n";
+																else 					echo "<option value='$element'>			$element</option>\n";
+															}
+															echo "</optgroup>";
+														?>
+													</select> <?php echo "</td>"
+									 . "<td>" ?> <input name="datum" type='text' value="<?php echo $datum;		?>")> <?php echo "</td>"
+									 . "<td>" ?> <input name="kommentar" type='text' value="<?php echo $kommentar;	?>")> <?php echo "</td>"
+									   ?> <input name="action" type='hidden' value="edit"> <?php echo 
+									  "<td>" . "<input type='submit' value='Okay'>" . "</td>"
+									 . "<td>" . "<a href='../../../Dropbox/Studium/Studium%20-%20Medieninformatik/!semester%2003/Multimedia_Engineering_I/Brainstorm/MME-Uebung-Tobias/Übung4/projekt4/route.php'>Abbrechen</a>" . "</td>"
+									 . "</tr>\n";
+							} else {
+								echo "<tr>"
+									 . "<td>" . $etappe		. "</td>"
+									 . "<td>" . $region		. "</td>"
+									 . "<td>" . $datum		. "</td>"
+									 . "<td>" . $kommentar	. "</td>"
+									 . "<td>" . "<a href='tmp_bearbeiten.php?id="	. (int)$id . "&action=edit"		. "'>Bearbeiten</a>"	. "</td>"
+									 . "<td>" . "<a href='tmp_delete.php?id="		. (int)$id . "&action=delete"	. "'>Löschen</a>"	. "</td>"
+									 . "</tr>\n";
+							}
+						}
+						$sql_abfrage->close();
+						$mysqli->close();
+			?>
+		</table>
+		</form>
+	</body>
+</html>
+<?php
+	} // Schließende Klammer der if-Bedingung (Sicherheitsabfrage)
+?>

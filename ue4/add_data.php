e5d4adf4af6cb58c2feab5f0a816ec6094560672
KingLeonidas90 <marlon.mattern5@googlemail.com>
2016-01-25 07:52:46 +0100

    Zeitformat

--- ue4/add_data.php
+++ ue4/add_data.php
@@ -15,29 +15,39 @@ $value = $_POST['Date'];
 $value2 = $_POST['Name'];
 $value3 = $_POST['Start'];
 $value4 = $_POST['End'];
-//Value 0 ist Gesamtzeit kann entfernt oder verändert werden
+//Value 0 ist Gesamtzeit kann entfernt oder verï¿½ndert werden
 $value0 = $value4-$value3;
 $value5 = $_POST['Subtext'];
 $value6 = $_POST['Location'];
 
 
-$time_diff = strtotime('12:45:00') - strtotime('11:00:00');
+$time_diff = strtotime('15:45:00') - strtotime('11:00:00');
 
 // Testausgabe in der Konsole
-echo "übergebene Zeiten: <br>";
-echo "start: ".$value3." ende: ".$value4."<br><br>";
+echo "vergebene Zeiten: <br>";
+echo "start: ".($value3)." ende: ".$value4."<br><br>";
 
-echo "Zeitunterschied gerechnet: ".$time_diff/60 . "<br>";
+echo "Zeitunterschied gerechnet: ".$time_diff/60/60 . "<br>";
 
 
 
+
+$time1 = $value3;
+$time2 = $value4;
+$timelength = strtotime( $time2 ) - strtotime( $time1 );
+
+$hours = intval( $timelength / 3600 );
+$minutes = intval( ( $timelength % 3600 ) / 60 );
+$seconds = $timelength % 60;
+
+echo str_pad( $hours, 2, '0', STR_PAD_LEFT ) . ':' . str_pad( $minutes, 2, '0', STR_PAD_LEFT ) . ':' . str_pad( $seconds, 2, '0', STR_PAD_LEFT );
 // SQL String um Daten in die DB zu schreiben
-// value0 ist für Gesamtzeit.
+// value0 ist fï¿½r Gesamtzeit.
 $mysqli->query("INSERT INTO workload (id, Datum, Bezeichnung, Start, Ende, Gesamt, Bemerkung, Ort) VALUES (null, '$value', '$value2',
-     '$value3', '$value4','$value0' , '$value5', '$value6')");
+     '$value3', '$value4',str_pad( $hours, 2, '0', STR_PAD_LEFT ) . ':' . str_pad( $minutes, 2, '0', STR_PAD_LEFT ) . ':' . str_pad( $seconds, 2, '0', STR_PAD_LEFT ) , '$value5', '$value6')");
 
 
 	$mysqli->close();
-// Wieder kommentar entfernen um zurück zur index.html zu wechseln
+// Wieder kommentar entfernen um zurï¿½ck zur index.html zu wechseln
 //	header("Location: http://$host$uri/$extra");
 ?>
